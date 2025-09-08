3289561e32741acfecd2e81c889acd7fa60ff9e2
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2025-09-08 09:32:19 +0530

    Time: 45 ms (71.56%), Space: 90.9 MB (32.08%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i: nums){
+            if(s.contains(i)){
+                return true;
+            }
+            else{
+                s.insert(i);
+            }
+        }
+        return false;
+    }
+};
