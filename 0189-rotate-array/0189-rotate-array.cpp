c44c901f20e2d0208cd23573b8a2c86083b97b03
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2025-11-04 22:51:58 +0530

    Time: 0 ms (100%), Space: 29.6 MB (36.3%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -11,22 +11,39 @@ public:
         // }
 
         //better
-         int n = nums.size();
-        k = k % n;
-        vector<int> temp(k);
-        int j = 0;
+        // int n = nums.size();
+        // k = k % n;
+        // vector<int> temp(k);
+        // int j = 0;
 
-        for (int i = n - k; i < n; i++) {
-            temp[j] = nums[i];
-            j++;
-        }
-        for (int i = n - 1; i >= k; i--) {
-            nums[i] = nums[i - k];
-        }
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
-        }
+        // for (int i = n - k; i < n; i++) {
+        //     temp[j] = nums[i];
+        //     j++;
+        // }
+        // for (int i = n - 1; i >= k; i--) {
+        //     nums[i] = nums[i - k];
+        // }
+        // for (int i = 0; i < k; i++) {
+        //     nums[i] = temp[i];
+        // }
         
-       
+       //optimal
+       int n=nums.size();
+       if(n<=0) return;
+        k = k % n;
+        if (k == 0) return;
+        reverse(0, n - k - 1, nums);
+        reverse(n - k, n - 1, nums);
+        reverse(0, n - 1, nums);
+
+    }
+    void reverse(int low,int high,vector<int>&nums){
+        while(low<=high){
+            int temp=nums[low];
+            nums[low]=nums[high];
+            nums[high]=temp;
+            low++;
+            high--;
+        }
     }
 };
