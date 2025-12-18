4731aa1b0cb0e24a5ecf0165dbda9d531f16a21e
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2025-12-18 18:58:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (31.87%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int idx=0;
+        for(int i=0;i<t.length();i++){
+            if(t[i]==s[idx]){
+                idx++;
+            }
+        }
+        if(idx==s.length()) return true;
+        return false;
+    }
+};
