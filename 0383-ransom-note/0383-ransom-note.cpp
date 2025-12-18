41c7a2b37b27a031c58fa59c9ac912fdcfc747b7
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2025-12-18 23:03:23 +0530

    Time: 4 ms (42.66%), Space: 12 MB (25.87%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<int,int> map;
+        for(int i=0;i<magazine.length();i++){
+            map[magazine[i]]++;
+        }
+        int c=0;
+        for(int i=0;i<ransomNote.length();i++){
+            if(map.find(ransomNote[i])!=map.end()){
+                if(map[ransomNote[i]]==0) continue;
+                c++;
+                map[ransomNote[i]]--;
+            }
+        }
+        if(c==ransomNote.length()) return true;
+        return false;
+    }
+};
