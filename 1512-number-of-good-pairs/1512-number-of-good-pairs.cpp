bfee4861e2a01d54770ecb0064e19877ffe5b2fd
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2026-03-11 18:55:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int c=0;
+        unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
-            for(int j=i;j<nums.size();j++){
-                if(nums[i]==nums[j] && i<j){
-                    c++;
-                }
-            }
+            mp[nums[i]]++;
+        }
+        int c=0;
+        for(auto i : mp){
+            int n=i.second;
+            c=c+n*(n-1)/2;
         }
         return c;
     }
