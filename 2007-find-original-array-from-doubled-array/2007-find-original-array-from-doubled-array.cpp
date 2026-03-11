4df2d6f1e2daf1b893e2e9d1b852860dc09a329c
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2026-03-11 23:27:30 +0530

    Time: 140 ms (58.91%), Space: 145.6 MB (56.06%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -12,7 +12,7 @@ public:
         for(int i=0;i<changed.size();i++){
             int sq=changed[i]*2;
             if(mp[changed[i]]==0) continue;//we lets say 1,2,3,4,6,8 for 1 2 is its square and afterwards we decrement bot of them so in map 1 and 2 both becomes 0 so when i comes to 2 we dont need to find the square of 2 because 2 has already been assigned as a sqare value of 1 so that's why skip the element 
-            if(!mp.count(sq) || mp.count(sq)==0){
+            if(!mp.count(sq) || mp[sq]==0){
                 return {};
             }
             result.push_back(changed[i]);
