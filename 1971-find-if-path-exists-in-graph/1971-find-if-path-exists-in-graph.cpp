e71519c8ab4ec08ae2c665144e0db5653c5a6072
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2026-03-17 02:31:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
+    bool helper(int source,int dest,vector<vector<int>> &edges,vector<bool> &visited){
+        if(source==dest) return true;
+        visited[source]=true;
+        // vector<int> neighbours=edges[source];
+        for(int i:edges[source]){
+            if(!visited[i]){
+                if(helper(i,dest,edges,visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> adj(n);
-        for (auto &e : edges) {
+        vector<bool> visited(n,false);
+        vector<vector<int>> adj(n);         // FIX: create adjacency list
+
+        for(auto &e:edges){                 // FIX: convert edge list to adjacency list
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
-
-        vector<bool> vis(n, false);
-        return helper(source, destination, adj, vis);
-    }
-
-    bool helper(int src, int dest, vector<vector<int>>& edges, vector<bool> &vis) {
-        if(edges.size()==1) return true;
-        vis[src] = true;
-        vector<int> neighbours = edges[src];
-        for (auto u : neighbours) {
-            if (u == dest) return true;
-            if (!vis[u] && helper(u, dest, edges, vis)) return true;
-        }
-        return false;
+        return helper(source,destination,adj,visited);
     }
 };
