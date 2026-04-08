cfcb36b63c846650eb17a42d897b34d114ad1318
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2026-04-08 22:38:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
         queue<pair<pair<int,int>,int>> q;
-        vector<vector<bool>> visited(grid.size(),vector<bool> (m,false));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
+        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(!visited[i][j] && grid[i][j]==2){
                     q.push({{i,j},0});
                     visited[i][j]=true;
                 }
             }
         }
-        int time=0;
+        vector<int> rr={-1,0,1,0};
+        vector<int> cc={0,1,0,-1};
+        int maxCount=0;
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
-            int t=q.front().second;
+            int count=q.front().second;
             q.pop();
-            time=max(time,t);
-            vector<int> neighR={-1,0,1,0};//top right bottom left
-            vector<int> neighC={0,1,0,-1};//top right bottom left
-            for(int i=0;i<4;i++){
-                int rr=r+neighR[i];
-                int cc=c+neighC[i];
-    if(rr>=0 && rr<n && cc>=0 && cc<m && !visited[rr][cc] && grid[rr][cc]==1)            {
-                    visited[rr][cc]=true;
-                    q.push({{rr,cc},t+1});
+            maxCount=max(maxCount,count);
+            for(int i=0;i<4;i++)
+            {
+                int newR=r+rr[i];
+                int newC=c+cc[i];
+                if(newR>=0 && newR<grid.size() && newC>=0 && newC<grid[0].size() && !visited[newR][newC] && grid[newR][newC]==1){
+                    // grid[newR][newC]=2;
+                    visited[newR][newC]=true;
+                    q.push({{newR,newC},count+1});
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(!visited[i][j] && grid[i][j]==1){
                     return -1;
                 }
             }
         }
-        return time;
+        return maxCount;
     }
 };
