c52ad5f63b9e1833648af593661612bae779fce6
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2026-04-10 23:14:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=0){
-                swap(nums[i],nums[j]);
+        int j=0,i=0;
+        while(j<nums.size()){
+            if(nums[j]!=0){
+                swap(nums[j],nums[i]);
+                j++;
+                i++;
+            }
+            else{
                 j++;
             }
         }
