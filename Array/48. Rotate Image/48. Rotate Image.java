e2efc9f16bcfa13572972c21562e430a2c775aa4
Pranay Deep <143511122+Pranay921@users.noreply.github.com>
2026-08-08 08:47:48 +0530

    CodePusher: Solution for 48. Rotate Image

---
+++ Array/48. Rotate Image/48. Rotate Image.java
@@ -0,0 +1,26 @@
+// Problem: 48. Rotate Image
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 43.6 MB (Beats 71.91%)
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            int left=0,right=matrix[0].length-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
